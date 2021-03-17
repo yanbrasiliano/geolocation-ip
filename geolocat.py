ecaccd3c5c9893dbe52cffabe18245dfa815f916
yanbrasiliano <yanpenabr@gmail.com>
2021-03-17 11:36:22 -0300

    Update geolocat.py

---
+++ geolocat.py
@@ -0,0 +1,22 @@
+""" 
+File name: 	geolocat.py		                
+Author:			Yan Brasiliano Silva Penalva	                       
+Email: 			yanpenabr@gmail.com
+---------------------------------------------------------------------
+Date created:								17 MAR 21                   
+Date last modified: 				17 MAR 21
+Python Version: 						Above 3.6 
+License: 										GNU License
+Maintainer: 								Yan Brasiliano Silva Penalva
+Version:										1.0
+---------------------------------------------------------------------
+Description: 
+PT-BR
+Este script recebe uma URL e retorna informações de sua localização e seu endereço IP.
+EN-US
+This script receives a URL and returns information about its location and its IP address. 
+"""
+
+
+
+
